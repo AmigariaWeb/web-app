9561485d75bb06eb1f93b650c0da9e5d70b441d1
Jesus Molano <jessumola@gmail.com>
2022-05-02 19:19:45 +0100

    Added auth with firebase

--- src/components/AppLogin/AppLoginGoogle.vue
+++ src/components/AppLogin/AppLoginGoogle.vue
@@ -1,27 +1,39 @@
 <script setup>
+import { useUserStore } from '@/stores/useUserStore';
 
+const userStore = useUserStore();
 </script>
 <template>
-        <RouterLink :to="{ path: '/'}">
-                <img
-                        className='google-icon'
-                        src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
-                        alt='google button'
-                    /> Entra con Google
-        </RouterLink>
+  <button @click="userStore.signInWithGoogle()" class="google-btn">
+    <img  className="google-icon"
+      src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+    <span>Entra con Google</span>
+  </button>
 </template>
 
 <style lang="scss" scoped>
-    .google-btn{
-        margin:auto;
-        display:inline-flex;
-        align-items: center;
-        justify-content: center;
-        background-color: inherit;
-        border:none;
-        text-decoration: none;
-    }
-    .google-icon{
-        height: 18px;
-    }
+.google-btn {
+  align-items: center;
+  background-color: var(--clr-emphasis-light);
+  border: none;
+  color: var(--clr-dark-blue-shadow);
+  display: flex;
+  padding: 1rem;
+  font-size: 1.2rem;
+  gap: 0.5rem;
+  justify-content: center;
+  margin: auto;
+  border-radius: 20px;
+  transition: background-color 0.3s ease;
+
+  &:hover,
+  &:focus {
+    background-color: var(--clr-yellow-light);
+    cursor: pointer;
+  }
+}
+
+.google-icon {
+  height: 18px;
+}
 </style>
