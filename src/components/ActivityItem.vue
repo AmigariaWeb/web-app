4fe60cf05e37a312dc3b5bfe4829a030104b52e1
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-05-10 12:11:04 +0200

    changed style in activityitem and activitydetailview

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -16,26 +16,33 @@ const saveActivity = () => {
 </script>
 
 <template>
+<div class="box">
   <div class="postit">
     <h3>{{ activity.title }}</h3>
     <p class="postItContent">{{ activity.description }}</p>
     <div class="postItData">
-      <p><strong>Tipo: </strong>{{ activity.type }}</p>
-      <p><strong>Cuándo: </strong>{{ activity.date }}</p>
-      <p><strong>Desde: </strong>{{ activity.from }}</p>
-      <p><strong>Hasta: </strong>{{ activity.to }}</p>
+      <!-- <p><strong>Tipo: </strong>{{ activity.type }}</p> -->
+      <p><!--<strong>Cuándo: </strong>-->{{ activity.date }}</p>
+      <!-- <p><strong>Desde: </strong>{{ activity.from }}</p>
+      <p><strong>Hasta: </strong>{{ activity.to }}</p> -->
+      <p><strong>Creado por:</strong> usuario</p>
     </div>
     <RouterLink to="/activityDetail" @click="saveActivity()" class="card">Saber más
     </RouterLink>
   </div>
+</div>
 </template>
 
 <style lang="scss" scoped>
+.box{
+  width: min(100%, 300px);
+  filter: drop-shadow(var(--shadow));
+}
 .postit {
   align-items: center;
   background-color: var(--clr-yellow-light);
   border-radius: 15px;
-  border: 15px solid white;
+  //border: 15px solid white;
   box-shadow: var(--shadow);
   color: var(--clr-blue-dark);
   display: flex;
@@ -47,58 +54,38 @@ const saveActivity = () => {
   position: relative;
   width: min(100%, 300px);
   transition: transform 0.5s ease;
+  clip-path: polygon(0% 0%,
+        0% 100%,
+        85% 100%,
+        100% 85%,
+        100% 0%);
 
   h3 {
     font-size: 1.5rem;
     text-align: center;
+    font-weight: bold;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
   }
 
-  &::before {
-    background-color: white;
-    bottom: -2px;
-    content: '';
-    display: flex;
-    height: 15%;
-    position: absolute;
-    right: -1px;
-    width: 50%;
-    clip-path: polygon(100% 44%,
-        100% 100%,
-        0% 100%,
-        43% 95.5%,
-        63% 84.5%,
-        77% 73%,
-        90% 60%);
-  }
 
   &::after {
-    background-color: #00000033;
-    bottom: -2px;
+    background-color: var(--clr-yellow-shadow);
+    border-radius: 15px 0 0 0;
+    bottom: 0;
     content: '';
     display: flex;
-    height: 15%;
     mix-blend-mode: multiply;
     position: absolute;
-    right: -1px;
-    width: 50%;
-    clip-path: polygon(88% 0%,
-        93% 11%,
-        97.5% 27%,
-        100% 44%,
-        90% 60%,
-        77% 73%,
-        63% 84.5%,
-        43% 95.5%,
-        0% 100%,
-        31% 91%,
-        50% 75.5%,
-        63.5% 58%,
-        76% 35.5%,
-        84% 15.5%);
+    right: 0;
+    width: 15%;
+    height: 15%;
   }
 
   &:hover {
-    transform: scale(1.015);
+    transform: scale(1.05);
   }
 
   & .postItContent {
@@ -106,32 +93,34 @@ const saveActivity = () => {
     -webkit-line-clamp: 3;
     -webkit-box-orient: vertical;
     overflow: hidden;
-  }
-
-  & .postItData {
     background-color: var(--clr-emphasis-light);
     border-radius: 0.5rem;
     padding: 0.5rem;
     margin: 0.5rem;
     width: 100%;
+    text-align: center;
+  }
+
+  & .postItData {
+    text-align: center;
   }
 
   & a {
     font-size: 1.5rem;
     width: min(100%, 150px);
-    border-radius: 50px;
-    border: 1px solid transparent;
+    border-radius: 20px;
     padding: 0.5rem;
     background-color: var(--clr-emphasis-light);
     color: var(--clr-green-dark);
     transition: background-color 0.5s ease, color 0.5s ease;
     text-align: center;
     text-decoration: none;
+    margin-top:0.5rem;
 
     &:hover {
       cursor: pointer;
       background-color: var(--clr-green-light);
-      color: var(--clr-emphasis-light);
+      color: var(--clr-dark-blue-shadow);
     }
   }
 }
--- src/views/ActivityDetailView.vue
+++ src/views/ActivityDetailView.vue
@@ -1,4 +1,3 @@
-
 <script setup>
 import { storeToRefs } from 'pinia';
 import { useActivitiesStore } from '../stores/useActivitiesStore';
@@ -13,46 +12,63 @@ const {addLastActivityDetail} = useActivitiesStore()
 
 </script>
 <template>
-  <main class="details">
-
+<div class="container">
+  <div class="details">
     <h3>{{ selectedActivity.title }}</h3>
     <div class="description">
-      <h4>Descripción de la actividad:</h4>
+      <!-- <h4>Descripción de la actividad:</h4> -->
       <p>{{ selectedActivity.description }}</p>
     </div>
-    <div class="type">
-      <h4>Tipo de actividad</h4>
-      <p>{{ selectedActivity.type }}</p>
-    </div>
-    <div class="calendar">
-      <p class="title-calendar">Calendario</p>
-      <p>
-        <strong>Fecha: </strong>
-        <span>{{ selectedActivity.date }}</span>
+    <p class="type">
+      <!-- <h4>Tipo de actividad</h4> -->
+      <p> <strong>Tipo:</strong> {{ selectedActivity.type }}</p>
+    </p>
+    <!-- <div class="calendar"> -->
+      <!-- <p class="title-calendar">Calendario</p> -->
+      <p class="date">
+        <strong>Fecha: </strong> <span>{{ selectedActivity.date }}</span>
       </p>
-      <p>
-        <strong>Desde las:</strong> <span>{{ selectedActivity.from }}</span>
-      </p>
-      <p>
-        <strong>Hasta las:</strong> <span>{{ selectedActivity.to }}</span>
-      </p>
-    </div>
+        <p class="from">
+          <strong>Desde</strong> <span>{{ selectedActivity.from }}</span>
+        </p>
+        <p class="to">
+          <strong>Hasta</strong> <span>{{ selectedActivity.to }}</span>
+        </p>
+
+    <!-- </div> -->
     <RouterLink to="/workinprogress">Me apunto</RouterLink>
-  </main>
+      <img src="https://picsum.photos/250/200" alt="">
+  </div>
+</div>
 </template>
 
 <style lang="scss" scoped>
+
+.container{
+  display:flex;
+  margin:1rem auto;
+  max-width: 700px;
+  justify-content: center;
+    transition: transform 0.5s ease;
+
+  &:hover{
+    transform: scale(1.05);
+  }
+}
 .details {
-  margin: 0 auto;
+  margin: 1rem;
   display: flex;
   flex-direction: column;
   padding: 1rem;
-  color: var(--clr-emphasis-light);
-  width: min(100%, 640px);
-  gap: 1.5rem;
+  color: var(--clr-dark-blue);
+  width: 100%;
+  background-color: var(--clr-yellow-light);
+  border-radius: 15px;
+  box-shadow: var(--shadow);
+  position:relative;
 
-  // :(
   h3 {
+    font-weight: bold;
     text-align: center;
   }
 
@@ -60,58 +76,53 @@ const {addLastActivityDetail} = useActivitiesStore()
     font-weight: 700;
   }
 
-  .description,
-  .type {
-    p {
-      border-top: 3px solid var(--clr-yellow-light);
-      padding-inline: 2rem;
-      color: var(--clr-emphasis-light);
-    }
-  }
-
-  .calendar {
-    background-color: var(--clr-emphasis-light);
-    width: min(100%, 200px);
-    border-radius: 20px;
+  .description{
     color: var(--clr-dark-blue);
+    background-color: white;
+    border-radius: 15px;
+    margin: 1rem;
     padding: 1rem;
-    align-self: center;
-    box-shadow: var(--shadow);
-
-    .title-calendar {
-      text-align: center;
-      border-bottom: 5px solid var(--clr-green-light);
-    }
-
-    strong {
-      color: var(--clr-dark-blue-shadow);
-    }
+    text-align: center;
   }
+
+  .date, .type, .from, .to{
+    margin-left: 2rem;
+  }
+
 
   a {
+    font-size:1.5rem;
     text-decoration: none;
     text-align: center;
     background-color: var(--clr-green-light);
     padding: 1rem;
-    width: min(100%, 300px);
-    align-self: center;
-    background-color: var(--clr-yellow-light);
+    width: min(100%, 200px);
     border: 3px solid transparent;
     border-radius: 20px;
-    font-size: 1.4rem;
     color: var(--clr-dark-blue-shadow);
-    transition: background-color 0.5s ease;
+    transition: background-color 0.5s ease, color 0.5s ease;
+    margin:1rem;
+
 
     &:hover {
-      background-color: var(--clr-yellow-shadow);
+      background-color: var(--clr-dark-blue);
+      color:white;
     }
+
+
+  }
+  img{
+    position:absolute;
+    bottom: 2rem;
+    right: 2rem;
+    border-radius: 15px;
   }
 }
 
 @media(min-width:640px) {
   .details {
     .calendar {
-      align-self: flex-end;
+      //align-self: flex-end;
     }
 
   }
