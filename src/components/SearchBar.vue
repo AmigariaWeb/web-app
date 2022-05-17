65a0fcd80caa8e88686cfbb3f1de3b8c4c5044a6
jesus-molano <jessumolano@gmail.com>
2022-05-17 11:47:06 +0100

    Added description and user filter in searchbar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,31 +1,39 @@
 <script setup>
-import { storeToRefs } from 'pinia';
-import { useActivitiesStore } from '../stores/useActivitiesStore';
+import { storeToRefs } from 'pinia'
+import { useActivitiesStore } from '../stores/useActivitiesStore'
+import { useWindowSize } from '@vueuse/core'
 
-const {searchQuery} = storeToRefs(useActivitiesStore());
-const {findSearchQuery} = useActivitiesStore();
-
+const { searchQuery } = storeToRefs(useActivitiesStore())
+const { findSearchQuery } = useActivitiesStore()
+const windowSize = useWindowSize()
 </script>
+
 <template>
   <div class="searchBarContainer">
-      <label for="searchBar">Buscar: </label>
-      <input id="searchBar" class="searchBar" type="search" placeholder="Título o tipo..." v-model="searchQuery" @keyup="findSearchQuery">
-    </div>
+    <label  for="searchBar">Buscar</label>
+    <input
+      id="searchBar"
+      class="searchBar"
+      type="search"
+      placeholder="Título, tipo, descrip..."
+      v-model="searchQuery"
+      @keyup="findSearchQuery"
+    />
+  </div>
 </template>
 
-
 <style lang="scss" scoped>
-.searchBarContainer{
+.searchBarContainer {
   margin-bottom: 1rem;
   display: flex;
   align-items: center;
   gap: 0.5rem;
   justify-content: flex-end;
 
-  label{
+  label {
     font-weight: 700;
     font-size: 1.2rem;
-    color: var( --clr-emphasis-light);
+    color: var(--clr-emphasis-light);
   }
   .searchBar {
     font-size: 1rem;
@@ -35,11 +43,23 @@ const {findSearchQuery} = useActivitiesStore();
     box-shadow: var(--shadow);
     color: var(--clr-dark-blue);
     transition: border 0.5s ease;
-  
-    &:hover, &:focus  {
+
+    &:hover,
+    &:focus {
       border: 3px solid var(--clr-yellow-light);
       outline: none;
     }
   }
+
 }
+  @media(max-width:640px){
+    .searchBarContainer{
+      flex-direction: column;
+      align-items: flex-end;
+      gap: 0;
+      label{
+        min-width: 195px;
+      }
+    }
+  }
 </style>
