d410a964d2f5ad2a58cc6aef7ffb787acf2cb209
Ian Montes <fotoianmontes@gmail.com>
2022-05-18 13:24:38 +0100

    PageNotFound view finished

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -6,61 +6,102 @@
 <main class="pageNotFound">
   <div id="container">
     <img id="svg" src="src/assets/images/amigaria_404.svg" alt="404: página no encontrada">
-    <h3 id="text1">Página no encontrada</h3>
-    <h4 id="text2">porque me la comí!</h4>
+    <div id="text">
+      <h3>Página no encontrada</h3>
+      <h4>porque me la comí!</h4>
+    </div>
+
+  </div>
+  <div class="links">
+    <span>¿Qué tal si pruebas por aquí?</span>
+    <div class="buttons">
+      <RouterLink class="navlink" to="/">
+        <button>Actividades</button>
+      </RouterLink>
+      <RouterLink class="navlink" to="/myactivities">
+        <button>Mis actividades</button>
+      </RouterLink>
+      <RouterLink class="navlink" to="/myactivities/form">
+        <button>Crear actividad</button>
+      </RouterLink>
+    </div>
   </div>
 </main>
 </template>
 
 <style lang="scss" scoped>
-.pageNotFound{
-  display: block;
-  align-content: center;
-  padding: 1rem; 
+  .pageNotFound{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem; 
+    gap: 3rem;
 
 
-#container{
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-}
+  #container{
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+  .links{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
 
-img{
-  width: 60vw;
-  max-width: 300px;
-}
+  .buttons{
+   display: flex;
+   flex-wrap: wrap;
+   gap: 1rem;
+   justify-content: center;
+  }
+
+  img{
+    width: 60vw;
+    max-width: 300px;
+  }
 
   h3{
     font-family: Pacifico;
     color: white;
+    text-align: center;
   }
   h4{
     font-family: Pacifico;
     color: white;
-  }
-  ul{
-    list-style: none;
-    margin: 0;
-    padding: 0;
     text-align: center;
-    a{
-      color: var(--clr-yellow-light);
-      font-size: 1.5rem;
-    }
-
   }
-
+  span {
+    font-size: 2rem;
+    color: white;
+    text-align: center;
+  }
+  button{
+    font-style: normal;
+		font-weight: 400;
+		font-size: 1.5625rem;
+		line-height: 2.34375rem;
+		text-align: center;
+		padding: 10px 10px;
+		border-radius: 20px;
+		background-color: var(--clr-yellow-light);
+		border: none;
+		transition: background-color 0.5s ease;
+		color: var(--clr-dark-blue);
+  }
 }
-// @media ()
-@media (max-width: 350px){
-  img{
-    width: 60vw;
-  }
+@media (min-width: 1000px){
   #container{
-    #text1 { order: 1; }
-    #text2 { order: 3; }
-    #svg { order: 2; }
+    flex-direction: row;
   }
+}
+@media (max-width: 999px){
+ #container{
+   flex-direction: column;
+   #text { order: 1; }
+   #svg { order: 2; }
+ }
 }
 </style>
