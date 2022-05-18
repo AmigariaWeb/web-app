35cab19b39c6ccb2ab7a2a8ef3a8876da7f1df63
Ian Montes <fotoianmontes@gmail.com>
2022-05-18 12:44:48 +0100

    Pacifico font added

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -4,27 +4,40 @@
 
 <template>
 <main class="pageNotFound">
-  <h3>No se ha encontrado lo que buscas</h3>
-  <h4>Quizás te referías a alguna de estas secciones:</h4>
-  <ul>
-    <li><RouterLink to="/">Actividades</RouterLink></li>
-    <li><RouterLink to="/workshops">Talleres</RouterLink></li>
-    <li><a href="https://amigaria.com">Apúntate</a></li>
-  </ul>
+  <div id="container">
+    <img id="svg" src="src/assets/images/amigaria_404.svg" alt="404: página no encontrada">
+    <h3 id="text1">Página no encontrada</h3>
+    <h4 id="text2">porque me la comí!</h4>
+  </div>
 </main>
 </template>
 
 <style lang="scss" scoped>
 .pageNotFound{
-  text-align: center;
-  display: grid;
-  place-content: center;
-  color: var(--clr-emphasis-light);
-  height: calc(90vh - var(--header-size));
-  padding: 1rem;
+  display: block;
+  align-content: center;
+  padding: 1rem; 
+
+
+#container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+}
+
+img{
+  width: 60vw;
+  max-width: 300px;
+}
 
   h3{
-    font-weight: 700;
+    font-family: Pacifico;
+    color: white;
+  }
+  h4{
+    font-family: Pacifico;
+    color: white;
   }
   ul{
     list-style: none;
@@ -38,5 +51,16 @@
 
   }
 
+}
+// @media ()
+@media (max-width: 350px){
+  img{
+    width: 60vw;
+  }
+  #container{
+    #text1 { order: 1; }
+    #text2 { order: 3; }
+    #svg { order: 2; }
+  }
 }
 </style>
