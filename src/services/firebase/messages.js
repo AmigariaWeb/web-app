e23d83616d2839e0904f927e89ab315b1cf93f57
jesus-molano <jessumolano@gmail.com>
2022-05-24 14:50:45 +0100

    Fixed realtime db problem

---
+++ src/services/firebase/messages.js
@@ -0,0 +1,55 @@
+
+
+import {  doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { ref, onUnmounted } from 'vue'
+import { db } from '@/services/firebase';
+import { storeToRefs } from 'pinia';
+
+export function useChat(userStore, activity) {
+  // Localstorage
+  const { user } = storeToRefs(userStore)
+  if (user.value === null) {
+    user.value = JSON.parse(localStorage.getItem('localUser'))
+  }
+
+  const activitiesDoc = doc(db, 'activities', activity.id)
+  const messages = ref([])
+  const refActivity = ref(null)
+  const unsubscribe = onSnapshot(activitiesDoc, (doc) => {
+    refActivity.value = doc.data()
+    refActivity.value.messages.length > 0
+      ? messages.value = (refActivity.value.messages)
+      : messages.value = ""
+  })
+  onUnmounted(unsubscribe)
+
+
+  const sendMessage = async text => {
+    const newMessage = {
+      image: user.value.image,
+      text: text,
+      userId: user.value.uid,
+      userName: user.value.name,
+      createdAt: new Date().toISOString()
+    }
+
+    user.value.joinedActivities.forEach(joinedActivity => {
+      if (joinedActivity.id === refActivity.value.id) {
+        refActivity.value.messages.push(newMessage)
+      }
+    })
+
+    user.value.userActivities.forEach(createdActivity => {
+      if (createdActivity.id === refActivity.value.id) {
+        refActivity.value.messages.push(newMessage)
+      }
+    })
+
+
+    const docRef = await updateDoc(activitiesDoc, {
+      messages: refActivity.value.messages
+    })
+  }
+
+  return { messages, sendMessage }
+}
