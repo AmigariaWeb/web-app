c239a697bacf09d5b08b3a1327f61a1064210b1f
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-05-25 16:14:22 +0200

    added spinner

--- src/views/FAQsView.vue
+++ src/views/FAQsView.vue
@@ -1,11 +1,12 @@
 <script setup>
-
+  import Spinner from '../components/Spinner/Spinner.vue'
 </script>
 
 <template>
   <main>
     <div>
       <h1>Preguntas frecuentes</h1>
+      <Spinner v-if="loading" />
       <details>
         <summary>Pregunta1</summary>
         <p>Respuesta1</p>
