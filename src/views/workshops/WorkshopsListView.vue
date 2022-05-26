61b5624453aeecc7a7aa5d78246780e61a5cdcda
Rommel Garcia <rommelgarcia4b@gmail.com>
2022-05-26 11:10:11 +0200

    condition to show title and workshop list added

--- src/views/workshops/WorkshopsListView.vue
+++ src/views/workshops/WorkshopsListView.vue
@@ -22,22 +22,25 @@ const { fetchWorkshops } = useWorkshopsStore();
 <template>
   <main>
     <Spinner v-if="loading" />
-    <p v-else-if="workshops.length === 0">Aun no hay talleres registrados</p>
-    <WorkshopList v-else v-for="(workshop, index) in workshops" :data="workshop" :index="index" :key="index"/>
-  
+    <h1 v-if="workshops.length === 0" class="workshops-content__title">Aun no hay talleres registrados</h1>
+    <h1 v-if="workshops.length > 0" class="workshops-content__title">Talleres</h1>
+    <div v-if="workshops.length > 0" class="workshops-content">
+      <WorkshopList v-for="(workshop, index) in workshops" :data="workshop" :index="index" :key="index"/>
+    </div>
     <PageInfoModal />
   </main>
 </template>
 
 <style lang="scss" scoped>
-  main{
+  .workshops{
+    &-content{
       display: flex;
       flex-flow: column;
       gap:60px;
-
-    h1{
-      text-align: center;
-      color: var(--clr-emphasis-light);
+      &__title{
+        text-align: center;
+        color: var(--clr-emphasis-light);
+      }
     }
   }
 </style>
